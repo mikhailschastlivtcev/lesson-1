14ce53014285b1dc1620db004b941596d9d4f816
Mikhail Schastlivcev <ghillghamesh@gmail.com>
2023-04-01 08:59:37 +0300

    pick 2 ex

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿/*
-Задача 2: Напишите программу,  которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
-*/
+
 
 int max = 0;
 int min = 0;
@@ -26,3 +26,42 @@ else
     min = firstNumber;
 }
 Console.WriteLine("max = " + max + " и " + "min = " + min);
+*/
+
+
+
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. (Сравнить с каждым)
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+
+*/
+
+int; max = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine("max = " + max);
